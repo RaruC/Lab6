8a80b3f34f5f2a0bea76e8c4891296d8bda82672
RaruC <91005152+RaruC@users.noreply.github.com>
2021-11-14 20:59:27 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+package Lab6;
+
+import Lab6.Organization;
+
+public class Main {
+    static public void main(String[] args)
+    {
+        Organization org = new Organization();
+
+        System.out.println(org);
+    }
+}
